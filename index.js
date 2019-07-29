3a1cc1d23bc006aea2a61bc3f991075e8f08428a
Abraham Gnanasingh <abraham.a@zohocorp.com>
2019-07-29 20:34:44 +0530

    Code changes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
-ReactDOM.render(
-    //<App />,
-    React.createElement(App, {}, null),
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     //<App />,
+//     React.createElement(App, {}, null),
+//     document.getElementById('root')
+// );
+
+function getReactCreateElement(component) {
+    debugger;
+    return React.createElement(component, {}, null);
+}
+
+var html = "<script>ReactDOM.render(getReactCreateElement(App), document.getElementById('root'));</script>";
+document.getElementById('renderScript').innerHTML = html;
