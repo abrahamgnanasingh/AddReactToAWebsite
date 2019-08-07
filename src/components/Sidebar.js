da33202a3cb7193004396c119cb5680af756eab6
Abraham Gnanasingh <abraham.a@zohocorp.com>
2019-08-07 16:41:19 +0530

    Code changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,18 +5,18 @@ class Sidebar extends React.Component {
         this.state = {
             menus: [
                 {
-                    id: 1, key: 'home', label: 'Home', isCollapsed: true, isCollapsing: false, subMenus: [
+                    id: 1, key: 'home', label: 'Home', isCollapsed: true, isCollapsing: false, offsetHeight: 0, subMenus: [
                         { id: 1.1, key: 'page1', label: 'Page 1' },
                         { id: 1.2, key: 'page2', label: 'Page 2' }
                     ]
                 },
                 {
-                    id: 2, key: 'settings', label: 'Settings', isCollapsed: true, isCollapsing: false, subMenus: [
+                    id: 2, key: 'settings', label: 'Settings', isCollapsed: true, isCollapsing: false, offsetHeight: 0, subMenus: [
                         { id: 2.1, key: 'general', label: 'General' },
                         { id: 2.2, key: 'personal', label: 'Personal' }
                     ]
                 },
-                { id: 3, key: 'about', label: 'About', isCollapsed: true, isCollapsing: false, subMenus: [] }
+                { id: 3, key: 'about', label: 'About', isCollapsed: true, isCollapsing: false, offsetHeight: 0, subMenus: [] }
             ]
         };
     }
@@ -29,6 +29,11 @@ class Sidebar extends React.Component {
         
     }
 
+    dimension($el) {
+        var hasWidth = $el.hasClass('width');
+        return hasWidth ? 'width' : 'height';
+    }
+
     handleDropdownToggle(e, menu, index) {
         if(menu.subMenus.length) {
             var collapseMenuEl = this.refs['collapseMenu' + menu.id];
@@ -39,6 +44,10 @@ class Sidebar extends React.Component {
                     isCollapsed = menus[i].isCollapsed;
                     menus[i].isCollapsed = !isCollapsed;
                     menus[i].isCollapsing = true;
+                    var $collapseMenuEl = $(collapseMenuEl);
+                    var dimension = this.dimension($collapseMenuEl);
+                    // var offsetHeight = $collapseMenuEl[dimension]($collapseMenuEl[dimension]())[0].offsetHeight;
+                    menus[i].offsetHeight = $collapseMenuEl[dimension]();
                 }
             }
             this.setState({
@@ -50,6 +59,7 @@ class Sidebar extends React.Component {
                 for(var i in menus) {
                     if(menus[i].key == menu.key) {
                         menus[i].isCollapsing = false;
+                        menus[i].offsetHeight = 0;
                     }
                 }
                 this.setState({
